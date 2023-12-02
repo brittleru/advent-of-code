f055957c97970835ef1d449e81d592f32dbfc7f0
Mocanu Sebastian <70840326+brittleru@users.noreply.github.com>
2023-12-02 20:28:12 +0200

    Day 02: Finished Task 2. Refactored the solution to respect DRY principle. Added tests for Task 2.

--- src/main/java/com/aoc/year2023/day02/Solution.java
+++ src/main/java/com/aoc/year2023/day02/Solution.java
@@ -3,6 +3,7 @@ package com.aoc.year2023.day02;
 import com.aoc.year2023.utils.FileProcessing;
 import com.aoc.year2023.utils.TextProcessing;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -21,27 +22,42 @@ public class Solution {
         String absolutePath = FileProcessing.getAbsolutePathFromUriString(puzzleResourceURI);
         List<String> puzzleData = FileProcessing.readFile(absolutePath);
 
-        int possibleGamesIdsSumPart1 = solution.solve(puzzleData, true);
-        System.out.println("\n\nThe sum of the IDs of the possible games is: " + possibleGamesIdsSumPart1);
+        int possibleGamesIdsSum = solution.solvePossibleGames(puzzleData, true);
+        int minimumCubesPower = solution.solveMinimumPower(puzzleData, true);
+        System.out.println("\n\nThe sum of the IDs of the possible games is: " + possibleGamesIdsSum);
+        System.out.println("The sum of the minimum powers of each game is: " + minimumCubesPower);
     }
 
-    public int solve(List<String> gamesData, boolean isVerbose) {
+    public int solveMinimumPower(List<String> gamesData, boolean isVerbose) {
+        int totalPower = 0;
+        if (isVerbose) {
+            System.out.println("\n--- Computing minimum possible power of Cubes ---");
+        }
+
+        for (String gameDataRow : gamesData) {
+            String[] cubeDataFromGame = processCubeDataFromGame(gameDataRow, isVerbose);
+            Map<String, Integer> minimumCubesPerColor = computeMinimumCubesPerColor(cubeDataFromGame);
+            int powerForCubes = computePowerForCubes(minimumCubesPerColor);
+            if (isVerbose) {
+                System.out.println("\t" + minimumCubesPerColor + " --- power: " + powerForCubes);
+            }
+            totalPower += powerForCubes;
+        }
+
+        return totalPower;
+    }
+
+
+    public int solvePossibleGames(List<String> gamesData, boolean isVerbose) {
         int possibleGamesIdsSum = 0;
         if (isVerbose) {
             System.out.println("\n--- Counting possible configurations of Cubes ---");
         }
 
         for (String gameDataRow : gamesData) {
-            String[] gameData = gameDataRow.split(":");
-            String gameId = gameData[0].split(" ")[1];
-            if (!TextProcessing.isInteger(gameId)) {
-                throw new RuntimeException("Invalid data format... Game ID must be placed after Game string...");
-            }
-            if (isVerbose) {
-                System.out.println(gameData[0] + " with ID -> " + gameId);
-            }
+            String gameId = gameDataRow.split(":")[0].split(" ")[1];
 
-            String[] cubeDataFromGame = gameData[1].split(";");
+            String[] cubeDataFromGame = processCubeDataFromGame(gameDataRow, isVerbose);
             int impossibleConfigurationsCounter = getImpossibilityConfigurationsNum(cubeDataFromGame, isVerbose);
             if (impossibleConfigurationsCounter == 0) {
                 possibleGamesIdsSum += Integer.parseInt(gameId);
@@ -49,6 +65,72 @@ public class Solution {
         }
 
         return possibleGamesIdsSum;
+    }
+
+    public String[] processCubeDataFromGame(String gameDataRow, boolean isVerbose) {
+        String[] gameData = gameDataRow.split(":");
+        String gameId = gameData[0].split(" ")[1];
+        if (!TextProcessing.isInteger(gameId)) {
+            throw new RuntimeException("Invalid data format... Game ID must be placed after Game string...");
+        }
+        if (isVerbose) {
+            System.out.println(gameData[0] + " with ID -> " + gameId);
+        }
+
+        return gameData[1].split(";");
+    }
+
+    public int computePowerForCubes(Map<String, Integer> minimumCubesPerColor) {
+        int power = 1;
+        for (Integer value : minimumCubesPerColor.values()) {
+            power *= value;
+        }
+
+        return power;
+    }
+
+    public Map<String, Integer> computeMinimumCubesPerColor(String[] cubeDataFromGame) {
+        Map<String, Integer> minimumNumbersOfCubesOfEachColor = new HashMap<>(Map.ofEntries(
+                Map.entry("red", 0),
+                Map.entry("green", 0),
+                Map.entry("blue", 0)
+        ));
+
+        for (String cubeData : cubeDataFromGame) {
+            String[] setsOfCubes = cubeData.split(",");
+            Map<String, Integer> setOfCubesData = getNumCubesOfSet(setsOfCubes);
+            for (Map.Entry<String, Integer> entry : minimumNumbersOfCubesOfEachColor.entrySet()) {
+                if (setOfCubesData.containsKey(entry.getKey())) {
+                    int setOfCubesNumber = setOfCubesData.get(entry.getKey());
+                    if (entry.getValue() < setOfCubesNumber) {
+                        minimumNumbersOfCubesOfEachColor.replace(entry.getKey(), setOfCubesNumber);
+                    }
+                }
+            }
+        }
+
+        return minimumNumbersOfCubesOfEachColor;
+    }
+
+    public Map<String, Integer> getNumCubesOfSet(String[] setOfCubes) {
+        Map<String, Integer> setOfCubesData = new HashMap<>();
+        for (String cubeData : setOfCubes) {
+            String[] cubeDataSplit = cubeData.strip().split(" ");
+
+            if (!TextProcessing.isInteger(cubeDataSplit[0])) {
+                throw new RuntimeException(
+                        "Invalid data format... Cube number must be first and then followed by Cube name");
+            }
+            int cubeNum = Integer.parseInt(cubeDataSplit[0]);
+            String cubeName = cubeDataSplit[1];
+
+            for (Map.Entry<String, Integer> entry : CUBES_THRESHOLD.entrySet()) {
+                if (cubeName.equals(entry.getKey())) {
+                    setOfCubesData.put(cubeName, cubeNum);
+                }
+            }
+        }
+        return setOfCubesData;
     }
 
     public int getImpossibilityConfigurationsNum(String[] cubeDataFromGame, boolean isVerbose) {
--- src/test/java/com/aoc/year2023/day02/SolutionTest.java
+++ src/test/java/com/aoc/year2023/day02/SolutionTest.java
@@ -7,8 +7,10 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest {
@@ -26,14 +28,57 @@ class SolutionTest {
     }
 
     @ParameterizedTest
-    @MethodSource("solverDataSource")
-    void testSolve(int expectedSumResult, String inputFilePath) {
+    @MethodSource("solveMinimumPowerDataSource")
+    void testSolveMinimumPower(int expectedPowerResult, String inputFilePath) {
         String absolutePath = FileProcessing.getAbsolutePathFromUriString(inputFilePath);
         List<String> puzzleData = FileProcessing.readFile(absolutePath);
 
-        int actualSumResult = solution.solve(puzzleData, false);
+        int actualPowerResult = solution.solveMinimumPower(puzzleData, false);
+
+        assertEquals(expectedPowerResult, actualPowerResult);
+    }
+
+    @ParameterizedTest
+    @MethodSource("solvePossibleGamesDataSource")
+    void testSolvePossibleGames(int expectedSumResult, String inputFilePath) {
+        String absolutePath = FileProcessing.getAbsolutePathFromUriString(inputFilePath);
+        List<String> puzzleData = FileProcessing.readFile(absolutePath);
+
+        int actualSumResult = solution.solvePossibleGames(puzzleData, false);
 
         assertEquals(expectedSumResult, actualSumResult);
+    }
+
+    @ParameterizedTest
+    @MethodSource("processCubeDataFromGameDataSource")
+    void testProcessCubeDataFromGame(String inputs, String[] expectedResult) {
+        String[] actualResult = solution.processCubeDataFromGame(inputs, false);
+
+        assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @ParameterizedTest
+    @MethodSource("computePowerForCubesDataSource")
+    void testComputePowerForCubes(Map<String, Integer> inputs, int expectedResult) {
+        int actualResult = solution.computePowerForCubes(inputs);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @ParameterizedTest
+    @MethodSource("computeMinimumCubesPerColorDataSource")
+    void testComputeMinimumCubesPerColor(String[] inputs, Map<String, Integer> expectedResult) {
+        Map<String, Integer> actualResult = solution.computeMinimumCubesPerColor(inputs);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @ParameterizedTest
+    @MethodSource("getNumCubesOfSetDataSource")
+    void testGetNumCubesOfSet(String[] inputs, Map<String, Integer> expectedResult) {
+        Map<String, Integer> actualResult = solution.getNumCubesOfSet(inputs);
+
+        assertEquals(expectedResult, actualResult);
     }
 
     @ParameterizedTest
@@ -60,10 +105,138 @@ class SolutionTest {
         assertEquals(expectedResult, actualResult);
     }
 
-    private static Stream<Arguments> solverDataSource() {
+    private static Stream<Arguments> solveMinimumPowerDataSource() {
+        return Stream.of(
+                Arguments.of(71220, inputFileURI),
+                Arguments.of(2286, testTask1URI)
+        );
+    }
+
+    private static Stream<Arguments> solvePossibleGamesDataSource() {
         return Stream.of(
                 Arguments.of(2377, inputFileURI),
                 Arguments.of(8, testTask1URI)
+        );
+    }
+
+    private static Stream<Arguments> processCubeDataFromGameDataSource() {
+        return Stream.of(
+                Arguments.of(
+                        "Game 1: 1 green, 1 blue, 1 red; 3 green, 1 blue, 1 red; 4 green, 3 blue, 1 red; 4 green, 2 blue, 1 red; 3 blue, 3 green",
+                        new String[]{
+                                " 1 green, 1 blue, 1 red",
+                                " 3 green, 1 blue, 1 red",
+                                " 4 green, 3 blue, 1 red",
+                                " 4 green, 2 blue, 1 red",
+                                " 3 blue, 3 green"
+                        }
+                ),
+                Arguments.of(
+                        "Game 20: 7 blue; 1 blue, 6 green, 1 red; 1 red, 3 blue, 10 green; 7 green, 4 blue, 1 red; 6 green, 6 blue, 1 red; 1 red, 5 blue, 17 green",
+                        new String[]{
+                                " 7 blue",
+                                " 1 blue, 6 green, 1 red",
+                                " 1 red, 3 blue, 10 green",
+                                " 7 green, 4 blue, 1 red",
+                                " 6 green, 6 blue, 1 red",
+                                " 1 red, 5 blue, 17 green"
+                        }
+                )
+        );
+    }
+
+    private static Stream<Arguments> computePowerForCubesDataSource() {
+        return Stream.of(
+                Arguments.of(Map.ofEntries(
+                        Map.entry("red", 53),
+                        Map.entry("green", 4),
+                        Map.entry("blue", 3)
+                ), 636),
+                Arguments.of(Map.ofEntries(
+                        Map.entry("red", 43),
+                        Map.entry("green", 4),
+                        Map.entry("blue", 3)
+                ), 516),
+                Arguments.of(Map.ofEntries(
+                        Map.entry("red", 1),
+                        Map.entry("green", 99),
+                        Map.entry("blue", 3)
+                ), 297),
+                Arguments.of(Map.ofEntries(
+                        Map.entry("red", 420),
+                        Map.entry("green", 420),
+                        Map.entry("blue", 420)
+                ), 74088000)
+        );
+    }
+
+    private static Stream<Arguments> computeMinimumCubesPerColorDataSource() {
+        return Stream.of(
+                Arguments.of(new String[]{
+                        " 1 green, 1 blue, 1 red",
+                        " 3 green, 1 blue, 1 red",
+                        " 4 green, 3 blue, 53 red",
+                        " 4 green, 2 blue, 1 red",
+                        " 3 blue, 3 green"}, Map.ofEntries(
+                        Map.entry("red", 53),
+                        Map.entry("green", 4),
+                        Map.entry("blue", 3)
+                )),
+                Arguments.of(new String[]{
+                        " 1 green, 1 blue, 1 red",
+                        " 3 green, 1 blue, 42 red",
+                        " 4 green, 3 blue, 42 red",
+                        " 4 green, 2 blue, 43 red",
+                        " 3 blue, 3 green"}, Map.ofEntries(
+                        Map.entry("red", 43),
+                        Map.entry("green", 4),
+                        Map.entry("blue", 3)
+                )),
+                Arguments.of(new String[]{
+                        " 1 green, 1 blue, 1 red",
+                        " 3 green, 1 blue, 1 red",
+                        " 4 green, 3 blue, 1 red",
+                        " 4 green, 2 blue, 1 red",
+                        " 3 blue, 99 green"}, Map.ofEntries(
+                        Map.entry("red", 1),
+                        Map.entry("green", 99),
+                        Map.entry("blue", 3)
+                )),
+                Arguments.of(new String[]{
+                        " 1 green, 420 blue, 1 red",
+                        " 3 green, 1 blue, 420 red",
+                        " 420 green, 3 blue, 1 red",
+                        " 4 green, 2 blue, 1 red",
+                        " 420 blue, 420 green"}, Map.ofEntries(
+                        Map.entry("red", 420),
+                        Map.entry("green", 420),
+                        Map.entry("blue", 420)
+                ))
+        );
+    }
+
+    private static Stream<Arguments> getNumCubesOfSetDataSource() {
+        return Stream.of(
+                Arguments.of(new String[]{" 1 green", " 1 blue", " 1 red"}, Map.ofEntries(
+                        Map.entry("red", 1),
+                        Map.entry("green", 1),
+                        Map.entry("blue", 1)
+                )),
+                Arguments.of(new String[]{" 45 green", " 81 blue", " 23 red"}, Map.ofEntries(
+                        Map.entry("red", 23),
+                        Map.entry("green", 45),
+                        Map.entry("blue", 81)
+                )),
+                Arguments.of(new String[]{" 5 green", " 5 blue", " 3 red"}, Map.ofEntries(
+                        Map.entry("green", 5),
+                        Map.entry("red", 3),
+                        Map.entry("blue", 5)
+                )),
+                Arguments.of(new String[]{" 42 green", " 42 blue", " 42 red"}, Map.ofEntries(
+                        Map.entry("green", 42),
+                        Map.entry("blue", 42),
+                        Map.entry("red", 42)
+                ))
         );
     }
 
